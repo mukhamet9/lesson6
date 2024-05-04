7b0a31c44735d74a88093883b01daf9bd77962d7
mukhamet9 <168105146+mukhamet9@users.noreply.github.com>
2024-05-04 16:38:56 +0300

    Add files via upload

---
+++ домашка6.py
@@ -0,0 +1,13 @@
+my_list = ['wolf', 'lion', 'eagle', 'horse']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:4:4])
+my_list[2] = 'shark'
+print(my_list)
+
+my_dict = {'wolf': 'волк', 'lion': 'лев', 'eagle': 'орел'}
+print(my_dict)
+print(my_dict['eagle'])
+my_dict['bear'] = 'медведь'
+print(my_dict)
